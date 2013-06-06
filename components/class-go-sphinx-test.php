17329a0b9c60b8b771f56d42660e97098252d08b
Will Luo <will.luo@gigaom.com>
2013-06-06 14:08:40 -0700

    added a link to run the tests on test-page.php and began to implement tests.

--- components/class-go-sphinx-test.php
+++ components/class-go-sphinx-test.php
@@ -15,8 +15,41 @@ class GO_Sphinx_Test extends GO_Sphinx
 			wp_die();
 		}
 
-print_r( go_sphinx()->client() );
-
+		echo "<pre>\n";
+		$this->wp_query_ten_most_recent_posts();
+		$this->sphinx_ten_most_recent_posts();
+		echo "</pre>\n";
 		die;
 	}
-}
+
+
+	public function wp_query_ten_most_recent_posts()
+	{
+		echo "WP_Query of ten most recent posts:\n\n";
+
+		$query = new WP_Query(
+			array(
+				'post_type'      => 'any',
+				'post_status'    => 'publish',
+				'posts_per_page' => 10,
+				'orderby'        => 'date', // or 'modified'?
+				'order'          => 'DESC',
+				'fields'         => 'ids',
+		) );
+
+		echo implode( ', ', $query->posts ) . "\n\n";
+		echo "done.\n";
+	}
+
+
+	public function sphinx_ten_most_recent_posts()
+	{
+		echo "Sphinx query of ten most recent posts:\n\n";
+
+		$client = $this->client();
+		//$client->SetLimits( 0, 10 )
+		//echo implode( ', ', $query->posts ) . "\n\n";
+		echo "done.\n";
+	}
+
+}//END GO_Sphinx_Test
