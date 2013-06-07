539863c979ef610c0c8c2cc775b00f2dc6055abc
Will Luo <will.luo@gigaom.com>
2013-06-07 13:31:49 -0700

    corrected naming of the test suite admin ajax name
    implemented most recent posts by most freqently used term test (2).

--- components/class-go-sphinx-test.php
+++ components/class-go-sphinx-test.php
@@ -2,9 +2,13 @@
 
 class GO_Sphinx_Test extends GO_Sphinx
 {
+	public $ten_most_recent_hits_wp  = FALSE;
+	public $ten_most_recent_hits_spx = FALSE;
+	public $taxonomies = FALSE; // all taxonomies in the system
+
 	public function __construct()
 	{
-		add_action( 'wp_ajax_go-sphinx-search-test', array( $this, 'search_test' ));
+		add_action( 'wp_ajax_go_sphinx_search_test', array( $this, 'search_test' ));
 	}
 
 	public function search_test()
@@ -16,30 +20,58 @@ class GO_Sphinx_Test extends GO_Sphinx
 		}
 
 		echo "<pre>\n";
+
 		$this->wp_query_ten_most_recent_posts();
 		$this->sphinx_ten_most_recent_posts();
+		echo "---\n\n";
+
+		if ( FALSE === ( $result = $this->get_most_used_term( $this->ten_most_recent_hits_wp ) ) )
+		{
+			echo "no term found for most recent posts by term tests\n\n";
+		}
+		else
+		{
+			$this->wp_query_most_recent_by_term( $result['post_id'], $result['term'] );
+			$this->sphinx_most_recent_by_term( $result['post_id'], $result['term'] );
+		}
 		echo "---\n\n";
 
 		echo "</pre>\n";
 		die;
 	}
 
-
 	public function wp_query_ten_most_recent_posts()
 	{
 		echo "WP_Query of ten most recent posts:\n\n";
 
-		$query = new WP_Query(
+		$results = new WP_Query(
 			array(
 				'post_type'      => 'any',
 				'post_status'    => 'publish',
 				'posts_per_page' => 10,
 				'orderby'        => 'date', // or 'modified'?
 				'order'          => 'DESC',
-				'fields'         => 'ids',
+
 		) );
 
-		echo implode( ', ', $query->posts ) . "\n\n";
+		if ( $results->posts )
+		{
+			$this->ten_most_recent_hits_wp = $results->posts;
+		}
+
+		if ( $this->ten_most_recent_hits_wp )
+		{
+			$ids = array();
+			foreach ( $this->ten_most_recent_hits_wp as $hit )
+			{
+				$ids[] = $hit->ID;
+			}
+			echo implode( ', ', $ids ) . "\n\n";
+		}
+		else
+		{
+			echo 'no post found';
+		}
 	}
 
 
@@ -51,12 +83,13 @@ class GO_Sphinx_Test extends GO_Sphinx
 		$client->SetLimits( 0, 10, 1000 );
 		$client->SetSortMode( SPH_SORT_EXTENDED, 'post_date_gmt DESC' );
 		$client->SetMatchMode( SPH_MATCH_EXTENDED );
-		$res = $client->Query( '@post_status publish' );
+		$results = $client->Query( '@post_status publish' );
 
-		if ( FALSE !== $res )
+		if ( FALSE !== $results )
 		{
+			$this->ten_most_recent_hits_spx = $results['matches'];
 			$hits = array();
-			foreach( $res['matches'] as $match )
+			foreach( $this->ten_most_recent_hits_spx as $match )
 			{
 				$hits[] = $match['id'];
 			}
@@ -67,6 +100,124 @@ class GO_Sphinx_Test extends GO_Sphinx
 			echo "query error: ";
 			print_r( $client->GetLastError() );
 			echo "\n\n";
+		}
+	}
+
+	// find the most frequently used term in the first post in $posts
+	// that contains any taxonomy. return a term object or FALSE if
+	// not found.
+	public function get_most_used_term( $posts )
+	{
+		$post_id = FALSE;    // set to first post with any taxonomy
+		$taxonomies = FALSE; // taxonomies associated with $post_id
+
+		foreach( $posts as $post )
+		{
+			$taxonomies = get_object_taxonomies( $post->post_type );
+			if ( ! empty( $taxonomies ) )
+			{
+				$post_id = $post->ID;
+				break;
+			}
+		}
+
+		$terms = wp_get_object_terms( $post_id, $taxonomies, array(
+										  'orderby' => 'count',
+										  'order'   => 'DESC',
+									) );
+		if ( is_wp_error( $terms ) )
+		{
+			print_r( $terms ) . "\n\n";
+			return FALSE;
+		}
+
+		// order = DESC seems to list count of 0 first so we have to
+		// make sure to skip posts with count of 0
+		foreach( $terms as $term )
+		{
+			if ( $term->count != 0 )
+			{
+				return array( 'post_id' => $post_id, 'term' => $term );
+			}
+		}
+		return FALSE;
+	}
+
+	public function wp_query_most_recent_by_term( $post_id, $term )
+	{
+		echo "WP_Query of ten most recent posts with ttid $term->term_taxonomy_id:\n\n";
+
+		$results = new WP_Query(
+			array(
+				'post_type'      => 'any',
+				'post_status'    => 'publish',
+				'posts_per_page' => 10,
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+				'tax_query'      => array(
+					array(
+						'taxonomy' => $term->taxonomy,
+						'field' => 'id',
+						'terms' => $term->term_id,
+						) )
+		) );
+
+		if ( empty( $results->posts ) )
+		{
+			echo "NULL\n";
+			return;
+		}
+
+		$ids = array();
+		foreach ( $results->posts as $hit )
+		{
+			$ids[] = $hit->ID;
+		}
+		echo implode( ', ', $ids ) . "\n\n";
+		if ( in_array( $post_id, $ids ) )
+		{
+			echo "source post ($post_id) found\n\n";
+		}
+		else
+		{
+			echo "source post ($post_id) NOT found\n\n";
+		}
+	}
+
+	public function sphinx_most_recent_by_term( $post_id, $term )
+	{
+		echo "\nSphinx query of ten most recent posts with ttid $term->term_taxonomy_id:\n\n";
+
+		$client = $this->client();
+		$client->SetLimits( 0, 10, 1000 );
+		$client->SetSortMode( SPH_SORT_EXTENDED, 'post_date_gmt DESC' );
+		$client->SetMatchMode( SPH_MATCH_EXTENDED );
+		$client->SetFilter( 'tt_id', array( $term->term_taxonomy_id ) );
+		$results = $client->Query( '@post_status publish' );
+
+		if ( FALSE === $results )
+		{
+			echo "query error: ";
+			print_r( $client->GetLastError() );
+			echo "\n\n";
+			return;
+		}
+
+		$ids = array();
+		foreach( $results['matches'] as $match )
+		{
+			$ids[] = $match['id'];
+		}
+
+		echo implode( ', ', $ids ) . "\n\n";
+
+		if ( in_array( $post_id, $ids ) )
+		{
+			echo "source post ($post_id) found\n\n";
+		}
+		else
+		{
+			echo "source post ($post_id) NOT found\n\n";
 		}
 	}
 
--- components/templates/test-page.php
+++ components/templates/test-page.php
@@ -3,5 +3,5 @@
 	<h2>Test Suite</h2>
 	<!-- <p><a href="<?php echo admin_url('admin-ajax.php?action=bsocial-test-twitter-api'); ?>">Twitter API tests</a></p> -->
 
-	 <p><a href="<?php echo admin_url('admin-ajax.php?action=go-sphinx-search-test'); ?>">Sphinx search tests</a></p>
+	 <p><a href="<?php echo admin_url('admin-ajax.php?action=go_sphinx_search_test'); ?>">Sphinx search tests</a></p>
 </div>
