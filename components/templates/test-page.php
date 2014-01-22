a25aa044d164a0ab294e76ba7d3a4491b175eb73
Will Luo <will.luo@gigaom.com>
2014-01-21 17:08:02 -0800

    added a link to the sphinxql test

--- components/templates/test-page.php
+++ components/templates/test-page.php
@@ -3,4 +3,6 @@
 	<h2>Test Suite</h2>
 
 	 <p><a href="<?php echo admin_url('admin-ajax.php?action=go_sphinx_search_test'); ?>">Sphinx search tests</a></p>
+
+	 <p><a href="<?php echo admin_url('admin-ajax.php?action=go_sphinx_ql_test'); ?>">SphinxQL tests</a></p>
 </div>
