123ff2ee9bc0e00ef78288bf616ab79da089d77d
Will Luo <will.luo@gigaom.com>
2014-07-31 17:00:59 -0700

    more consistent comment block

--- components/class-go-sphinx.php
+++ components/class-go-sphinx.php
@@ -70,8 +70,8 @@ class GO_Sphinx
 		'tag_slug__and',
 		'tax_query',
 
-		/* these are allowed to pass through either because we allow them
-		   or because they're alternate keys used by WP */
+		// these are allowed to pass through either because we allow them
+		// or because they're alternate keys used by WP
 		'exclude',
 		'feed',
 		'fields',
