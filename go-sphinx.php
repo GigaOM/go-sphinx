20418ecdc813876b3eb5aadfdde0df097dfb925d
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:41:56 -0400

    Update go-sphinx.php

--- go-sphinx.php
+++ go-sphinx.php
@@ -3,8 +3,11 @@
  * Plugin Name: Gigaom Sphinx Search Integration
  * Plugin URI: http://kitchen.gigaom.com/
  * Version: 0.1
- * Author: Casey Bisson
- * Author URI: http://maisonbisson.com/blog/
+ * Author: Gigaom
+ * Author URI: https://gigaom.com
+ * Contributors: misterbisson, okredo, zbtirrell, wluo, camwyn
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // include required components
